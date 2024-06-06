1cca58eb53f32a7d3437e6954fad8a5ae5b19856
bjoernbuth <bjoernbuth76@gmail.com>
2024-06-06 18:30:54 +0200

    generated first example

---
+++ semi_notebooks/scratch.py
@@ -0,0 +1,43 @@
+# %%
+
+from pathlib import Path
+
+# %%
+from drawpyo.diagram_types import TreeDiagram
+
+
+file_path = Path(__file__).parent
+
+tree = TreeDiagram(
+    file_path=file_path,
+    file_name="Tree Name.drawio",
+)
+
+# %%
+from drawpyo.diagram_types import NodeObject
+
+# Top object
+grinders = NodeObject(
+    tree=tree, value="Appliances for Grinding Coffee", base_style="rounded rectangle"
+)
+
+# Main categories
+blade_grinders = NodeObject(tree=tree, value="Blade Grinders", parent=grinders)
+burr_grinders = NodeObject(tree=tree, value="Burr Grinders", parent=grinders)
+blunt_objects = NodeObject(tree=tree, value="Blunt Objects", parent=grinders)
+
+# %%
+# Other
+elec_blade = NodeObject(tree=tree, value="Electric Blade Grinder", parent=blade_grinders)
+mnp = NodeObject(tree=tree, value="Mortar and Pestle", parent=blunt_objects)
+
+# Conical Burrs
+conical = NodeObject(tree=tree, value="Conical Burrs", parent=burr_grinders)
+elec_conical = NodeObject(tree=tree, value="Electric", parent=conical)
+manual_conical = NodeObject(tree=tree, value="Manual", parent=conical)
+
+# %%
+tree.auto_layout()
+tree.write()
+
+# %%
